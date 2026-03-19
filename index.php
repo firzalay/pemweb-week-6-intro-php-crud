4bfd2d0faaf8abbb1c8dc1e218cf55ded291ad02
firzalay <firza7004@gmail.com>
2026-03-19 21:21:53 +0700

    feat: init tailwindcss

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Mahasiswa</title>
+    <link rel="stylesheet" href="./public/src/css/output.css">
 </head>
 <body>
-    
+    <h1 class="text-3xl">Hello World</h1>
 </body>
 </html>
