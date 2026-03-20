fc538e43512463b7497fb4c90a67e373b7126643
firzalay <firza7004@gmail.com>
2026-03-21 04:56:43 +0700

    feat: add sidebar component

---
+++ public/components/Sidebar.php
@@ -0,0 +1,25 @@
+<aside class="bg-white border-r border-gray-200 ">
+      <nav class="min-h-screen flex flex-col justify-between">
+        <div class="p-4">
+            <div>
+              <h1 class="mb-8 text-xl font-bold">Data Mahasiswa</h1>
+            </div>
+            <ul class="flex flex-col gap-4 [&>li]:flex [&>li]:flex-col [&>li>a]:flex [&>li>a]:items-center [&>li>a]:gap-4 text-sm font-light">
+              <!-- <li>
+                <a href=""><i data-lucide="home" stroke-width="1.5"></i> Dashboard</a>
+              </li> -->
+              <li class="bg-gray-200 p-2 rounded-md">
+                <a href="" class="text-blue-600"><i data-lucide="book-user" stroke-width="1.5"></i>Manajemen
+                  Mahasiswa</a>
+              </li>
+              <li class="p-2">
+                <a href=""><i data-lucide="book-type" stroke-width="1.5" class="text-gray-400"></i><span class="text-gray-600">Manajemen Dosen</span></a>
+              </li>
+            </ul>
+        </div>
+        <div class="flex items-center gap-4 p-2 hover:bg-gray-200 cursor-pointer group">
+            <img src="./public/src/images/image.png" alt="Profile Image" class="w-15 h-15 rounded-full objcet-cover">
+            <h1 class="text-gray-600 font-bold group-hover:text-blue-600">John Doe</h1>
+        </div>
+      </nav>
+</aside>
