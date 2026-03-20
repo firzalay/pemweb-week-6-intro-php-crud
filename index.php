0d583074a3314cbaee4be155b1fbb6917f603ee8
firzalay <firza7004@gmail.com>
2026-03-20 22:40:15 +0700

    fix: change bottom profile of sidebar

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
 <body class="font-inter">
   <div class="flex min-h-screen">
     <aside class="bg-white border-r border-gray-200 ">
-      <nav class="min-h-screen p-4 flex flex-col justify-between">
-        <div>
+      <nav class="min-h-screen flex flex-col justify-between">
+        <div class="p-4">
             <div>
               <h1 class="mb-8 text-xl font-bold">Data Mahasiswa</h1>
             </div>
@@ -29,8 +29,8 @@
               </li>
             </ul>
         </div>
-        <div class="flex items-center gap-4 p-2 rounded-md hover:bg-gray-200 cursor-pointer group">
-            <img src="./public/src/images/image.png" alt="Profile Image" class="w-15 h-15 rounded-full">
+        <div class="flex items-center gap-4 p-2 hover:bg-gray-200 cursor-pointer group">
+            <img src="./public/src/images/image.png" alt="Profile Image" class="w-15 h-15 rounded-full objcet-cover">
             <h1 class="text-gray-600 font-bold group-hover:text-blue-600">John Doe</h1>
         </div>
       </nav>
